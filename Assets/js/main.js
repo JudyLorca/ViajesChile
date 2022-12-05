36ccd3f65515ef9c9c2098a416655fb22402de43
Judy Lorca <jlorca@hotmail.com>
2022-12-05 00:03:11 -0300

    Termino Proyecto Viajes Chile Incluye Ajustes finales

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,11 +1,14 @@
+/* Función Tooltip a íconos redes sociales */
+
 $(function (){
     $(`[data-bs-toggle="tooltip"]`).tooltip();
-}
-)
+})
+
+
+/* Condición solicitada, cambio color navbar en desplazamiento hacia parte inferior página */
 
 $(document).scroll(function () {
     const y = $("html").scrollTop();
-
     y > 300 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black");
 
 });
